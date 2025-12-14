9ac33263de28728a304310e92c0dc580c8946103
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2025-12-15 00:24:40 +0530

    all setup configer

--- src/app.js
+++ src/app.js
@@ -11,5 +11,14 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+// routes  
+
+import userRouter from  "./routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users" , userRouter)
+
 export { app }
